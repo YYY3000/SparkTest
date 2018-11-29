7aeace8726394152a4d17d64c5e25191ec9e4605
yyy3000 <15605542112@163.com>
2018-11-29 19:05:09 +0800

    新增fromRedisByteHash方法入口

--- src/main/java/RedisTest.java
+++ src/main/java/RedisTest.java
@@ -1,10 +1,9 @@
 import com.redislabs.provider.redis.RedisConfig;
 import com.redislabs.provider.redis.RedisContext;
 import com.redislabs.provider.redis.RedisEndpoint;
-import com.redislabs.provider.redis.rdd.RedisKeysRDD;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
-import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.rdd.RDD;
 import scala.Tuple2;
 
 /**
@@ -15,23 +14,20 @@ public class RedisTest {
 
     public static void main(String[] args) {
         SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("redis test");
-        sparkConf.set("redis.host", "hadoop-dn01");
+        sparkConf.set("redis.host", "192.168.1.16");
         sparkConf.set("redis.port", "6379");
+        sparkConf.set("redis.db", "3");
         sparkConf.set("redis.auth", "mastercom");
         SparkContext sparkContext = new SparkContext(sparkConf);
         RedisContext redisContext = new RedisContext(sparkContext);
         RedisConfig redisConfig = new RedisConfig(new RedisEndpoint(sparkConf));
-        RedisKeysRDD keysRDD = redisContext.fromRedisKeyPattern("sys_*", 5, redisConfig);
-        JavaRDD<Tuple2<String, String>> hashRdd = keysRDD.getHash().toJavaRDD();
-        System.out.println(hashRdd.count());
-        hashRdd.foreach(value -> {
+        RDD<Tuple2<byte[], byte[]>> redisRdd = redisContext.fromRedisByteHash("yyy_*", 5, redisConfig);
+        System.out.println(redisRdd.count());
+        redisRdd.toJavaRDD().foreach(value -> {
             System.out.println(value._1);
             System.out.println(value._2);
-        });
-        JavaRDD<String> listRdd =  keysRDD.getList().toJavaRDD();
-        System.out.println(listRdd.count());
-        listRdd.foreach(value -> {
-            System.out.println(value);
+            System.out.println(new String(value._1));
+            System.out.println(new String(value._2));
         });
         sparkContext.stop();
     }
